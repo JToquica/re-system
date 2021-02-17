672c3f8fbf47e7803465858796e33c7e9010b3ad
José Herley Toquica Montealegre <44205043+JToquica@users.noreply.github.com>
2021-02-16 21:39:00 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import turicreate as tc
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    
+    data = tc.SFrame({'user_id': ["Ann", "Ann", "Ann", "Brian", "Brian", "Brian"],
+                          'item_id': ["Item1", "Item2", "Item4", "Item2", "Item3", "Item5"],
+                          'rating': [1, 3, 2, 5, 4, 2]})
+    m = tc.factorization_recommender.create(data, target='rating')
+
+    recommendations = m.recommend()
+    return(recommendations)
