bc60d93636f9f3e282a02ecbda69e016b0f3dabc
José Herley Toquica Montealegre <44205043+JToquica@users.noreply.github.com>
2021-02-17 07:11:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,7 @@ app = Flask(__name__)
 @app.route("/", methods=["GET"])
 def hello():
     
-    data = tc.SFrame({'user_id': ["Ann", "Ann", "Ann", "Brian", "Brian", "Brian"],
-                          'item_id': ["Item1", "Item2", "Item4", "Item2", "Item3", "Item5"],
-                          'rating': [1, 3, 2, 5, 4, 2]})
-    m = tc.factorization_recommender.create(data, target='rating')
+    return 'Hello World'
 
-    recommendations = m.recommend()
-    return(recommendations)
+if __name__ == '__main__':
+    app.run()
