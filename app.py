bf3be48f36f0985e5cfe66510b8069c0e7fa0595
José Herley Toquica Montealegre <44205043+JToquica@users.noreply.github.com>
2021-02-21 11:30:35 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,28 +1,29 @@
 from flask import Flask
 from flask_cors import CORS
+import turicreate as tc
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
-@app.route('/html')
-def hello_world_html():
-    return '<h1>Hello, World!</h1>'
-
-@app.route('/json')
-def hello_world_json():
-    return {"message":"Hello, World!"}
-
-@app.route('/json/list')
+@app.route('/recomendaciones')
 def hello_world_json_list():
-	data = []
-	data.append({"name":"Pedro", "age":24})
-	data.append({"name":"Maria", "age":18})
-	data.append({"name":"Jose", "age":31})
-	return {"data":data}
+
+        calificacion = {'user_id': ["Ann", "Ann", "Ann", "Brian", "Brian", "Brian"],
+                          'item_id': ["Item1", "Item2", "Item4", "Item2", "Item3", "Item5"],
+                          'rating': [1, 3, 2, 5, 4, 2]}
+
+        data = tc.SFrame(calificacion)
+
+        m = tc.factorization_recommender.create(data, target='rating')
+
+        recommendations = m.recommend(users=['Brian'])
+
+        datos = []
+
+        for i in recommendations:
+                datos.append(i)
+
+        return {"data":datos}
 
 if __name__ == '__main__':
-    app.run()
+    app.run
